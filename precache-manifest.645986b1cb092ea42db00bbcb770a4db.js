86453d322395156817f96e9075648a783b07c476
Xin Zheng <jason.zhengxin@gmail.com>
2019-10-20 18:05:54 +1100

    Updates

---
+++ precache-manifest.645986b1cb092ea42db00bbcb770a4db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a1c044845c7d4f81493e0811ae57487",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "13501ec106e0f00518eb",
+    "url": "/monster-rolodex/static/css/main.49088b4f.chunk.css"
+  },
+  {
+    "revision": "b0703eff4582a78f493a",
+    "url": "/monster-rolodex/static/js/2.5075ebff.chunk.js"
+  },
+  {
+    "revision": "13501ec106e0f00518eb",
+    "url": "/monster-rolodex/static/js/main.7c0c08fe.chunk.js"
+  },
+  {
+    "revision": "ec2849d1eae557c66f4a",
+    "url": "/monster-rolodex/static/js/runtime-main.b891af59.js"
+  }
+]);
